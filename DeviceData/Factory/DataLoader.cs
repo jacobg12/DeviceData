8f9fd489bca463b084c4195e3d877b4bf903f194
jacobg12 <jacob.gaiski@comcast.net>
2023-07-31 14:37:57 -0400

    Remove unncessary using statements

--- DeviceData/Factory/DataLoader.cs
+++ DeviceData/Factory/DataLoader.cs
@@ -1,13 +1,8 @@
 ﻿using DeviceData.Entities;
 using DeviceData.Interfaces;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeviceData.Factory
 {
--- DeviceData/Factory/DeviceHub.cs
+++ DeviceData/Factory/DeviceHub.cs
@@ -1,11 +1,8 @@
 ﻿using DeviceData.Entities;
 using DeviceData.Interfaces;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 
 namespace DeviceData.Factory
 {
